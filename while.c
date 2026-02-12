1e170cdf385904408585a568ff02739189e02833
cjdreamy <cjdreamy402@gmail.com>
2026-02-12 08:49:18 +0300

    added while.C

---
+++ while.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+    int i = 0;
+    while (i < 10){
+        printf("\ni am executing the while loop for the %d time\n", i );
+        i++;
+    }
+    return 0;
+}
